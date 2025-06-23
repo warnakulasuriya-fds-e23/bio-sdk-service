b5b1966c790898e6d7c22a08f3890509a4c27436
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-06-23 09:07:33 +0530

    identifyTemplate function

--- controller/fingerprintcontroller/identifyTemplate.go
+++ controller/fingerprintcontroller/identifyTemplate.go
@@ -1,7 +1,25 @@
 package fingerprintcontroller
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/warnakulasuriya-fds-e23/biometric-orchestration-go-server/requestobjects"
+	"github.com/warnakulasuriya-fds-e23/biometric-orchestration-go-server/responseobjects"
+)
 
 func (controller *fingerprintController) identifyTemplate(c *gin.Context) {
-
+	var reqObj requestobjects.IdentifyTemplateReqObj
+	err := c.BindJSON(&reqObj)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	probeTemplate := controller.sdk.ParseByteArrayToTemplate(&reqObj.ProbeCbor)
+	isMatched, discoveredId := controller.sdk.Identify(probeTemplate)
+	resObj := responseobjects.IdentifyTemplateResObje{
+		IsMatched:    isMatched,
+		DiscoveredId: discoveredId,
+	}
+	c.IndentedJSON(http.StatusOK, resObj)
 }
