d5d145a6d51096c3ea1304527bf256256f71344d
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-06-24 14:26:58 +0530

    added working directory path to error response string

--- controller/gallerycontroller/updateCborDir.go
+++ controller/gallerycontroller/updateCborDir.go
@@ -2,6 +2,7 @@ package gallerycontroller
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/warnakulasuriya-fds-e23/biometric-orchestration-go-server/requestobjects"
@@ -12,12 +13,22 @@ func (controller *galleryController) updateCborDir(c *gin.Context) {
 	var reqObj requestobjects.UpdateCborDirReqObj
 	err := c.BindJSON(&reqObj)
 	if err != nil {
-		resObj := responseobjects.ErrorResObj{Message: err.Error()}
+		workingDir, errGetwd := os.Getwd()
+		if errGetwd != nil {
+			resObj := responseobjects.ErrorResObj{Message: errGetwd.Error() + " + " + err.Error()}
+			c.IndentedJSON(http.StatusInternalServerError, resObj)
+		}
+		resObj := responseobjects.ErrorResObj{Message: "Current working directory: " + workingDir + " ," + err.Error()}
 		c.IndentedJSON(http.StatusInternalServerError, resObj)
 	}
 	message, err := controller.sdk.UpdateCborDir(reqObj.PathString)
 	if err != nil {
-		resObj := responseobjects.ErrorResObj{Message: err.Error()}
+		workingDir, errGetwd := os.Getwd()
+		if errGetwd != nil {
+			resObj := responseobjects.ErrorResObj{Message: errGetwd.Error() + " + " + err.Error()}
+			c.IndentedJSON(http.StatusInternalServerError, resObj)
+		}
+		resObj := responseobjects.ErrorResObj{Message: "Current working directory: " + workingDir + " ," + err.Error()}
 		c.IndentedJSON(http.StatusInternalServerError, resObj)
 	}
 	resObj := responseobjects.UpdateCborDirResObj{Message: message}
--- controller/gallerycontroller/updateImagesDir.go
+++ controller/gallerycontroller/updateImagesDir.go
@@ -2,6 +2,7 @@ package gallerycontroller
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/warnakulasuriya-fds-e23/biometric-orchestration-go-server/requestobjects"
@@ -12,12 +13,22 @@ func (controller *galleryController) updateImagesDir(c *gin.Context) {
 	var reqObj requestobjects.UpdateImagesDirReqObj
 	err := c.BindJSON(&reqObj)
 	if err != nil {
-		resObj := responseobjects.ErrorResObj{Message: err.Error()}
+		workingDir, errGetwd := os.Getwd()
+		if errGetwd != nil {
+			resObj := responseobjects.ErrorResObj{Message: errGetwd.Error() + " + " + err.Error()}
+			c.IndentedJSON(http.StatusInternalServerError, resObj)
+		}
+		resObj := responseobjects.ErrorResObj{Message: "Current working directory: " + workingDir + " ," + err.Error()}
 		c.IndentedJSON(http.StatusInternalServerError, resObj)
 	}
 	message, err := controller.sdk.UpdateImageDir(reqObj.PathString)
 	if err != nil {
-		resObj := responseobjects.ErrorResObj{Message: err.Error()}
+		workingDir, errGetwd := os.Getwd()
+		if errGetwd != nil {
+			resObj := responseobjects.ErrorResObj{Message: errGetwd.Error() + " + " + err.Error()}
+			c.IndentedJSON(http.StatusInternalServerError, resObj)
+		}
+		resObj := responseobjects.ErrorResObj{Message: "Current working directory: " + workingDir + " ," + err.Error()}
 		c.IndentedJSON(http.StatusInternalServerError, resObj)
 	}
 	resObj := responseobjects.UpdateImagesDirResObj{Message: message}
