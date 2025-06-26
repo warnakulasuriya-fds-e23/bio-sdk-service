3fcd362be949abce03b36a46c8cd40698d6036f6
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-06-26 11:45:41 +0530

    used config toml reader in initializer

--- initializer/initializer.go
+++ initializer/initializer.go
@@ -2,23 +2,13 @@ package initializer
 
 import (
 	"log"
-	"os"
-	"path/filepath"
 
-	"github.com/BurntSushi/toml"
-	"github.com/warnakulasuriya-fds-e23/biometric-orchestration-go-server/configuration"
+	"github.com/warnakulasuriya-fds-e23/biometric-orchestration-go-server/configtomlreader"
 	"github.com/warnakulasuriya-fds-e23/fingerprint-go-sdk/core"
 )
 
 func Initializer() (sdk *core.SDKCore) {
-	workingDir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	tomlpath := filepath.Join(workingDir, "config.toml")
-	var config configuration.Configuration
-	toml.DecodeFile(tomlpath, &config)
+	config := configtomlreader.ConfigTomlReader()
 	s, err := core.NewSDKCore(config.ImagesDir, config.CborDir)
 	if err != nil {
 		log.Fatal(err.Error())
